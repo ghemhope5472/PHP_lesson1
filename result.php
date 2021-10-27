84062db6cb2b20225236cf5af18ece6f88a39ece
ghemhope5472 <ghemhope@gmail.com>
2021-10-27 13:57:26 +0800

    Update result.php

--- result.php
+++ result.php
@@ -5,13 +5,35 @@
    if(empty($_GET['search'])){
        echo "No entry";
    } else{
+
        $check = $_GET['search'];
+       $q = "SELECT * FROM mytbl WHERE ";
+       $i = 0;
+       $terms = explode(" ", $check);
 
-       $terms = explode(' ', $check);
        foreach($terms as $each){
-           echo $each . "<br>";
-       }
-
+           $i++;
+           
+           if($i == 1){
+                $q .= "name LIKE '%$each%' ";
+             
+           }else{
+               $q .= "OR name LIKE '%$each%' ";
+               
+           }
    }
+   $query = mysqli_query($connections, $q);
+   $c_q = mysqli_num_rows($query);
 
+  
+   if($c_q > 0 && $check != ""){
+        while($row = mysqli_fetch_assoc($query)){
+            echo $name = $row['name'] . "<br>";
+        }
+   }else{
+       echo "No result";
+   }
+   
+}
+   
 ?>
